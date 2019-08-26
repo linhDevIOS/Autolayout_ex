8830b95c1ccad43b521cf5ad0ab006323136dc6c
linhDevIOS <linhappcare@gmail.com>
2019-08-26 15:10:35 +0700

    Initial Commit

---
+++ Autolayout_ex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout_ex
+//
+//  Created by hoanglinh on 8/26/19.
+//  Copyright © 2019 hoanglinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
